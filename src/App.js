d8ff93261e3136d3cfe04162ddffcaa52936a2b4
michael dilulo <snoleopard01001101@gmail.com>
2020-11-30 17:52:59 -0800

    API Request, populated Select List

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import { FormControl, Select, MenuItem } from '@material-ui/core'
+import { useEffect, useState } from 'react';
 
 function App() {
+  // State = How to write a variable in React
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }))
+          setCountries(countries)
+      })
+    }
+    getCountriesData()
+  }, [])
+
+
   return (
     <div className="App">
+        {/* Header */}
       <div className="app__header">
       <h1>COVID-19 TRACKER</h1>
       <FormControl className="app__dropdown">
         <Select 
-          variant="outlined" value='abc'>
-          
-          <MenuItem value="worldwide">WorldWide</MenuItem>
-          <MenuItem value="worldwide">Option Two</MenuItem>
-          <MenuItem value="worldwide">Option Three</MenuItem>
-          <MenuItem value="worldwide">Option Four</MenuItem>
+            variant="outlined" value='abc'>
+
+              {/* Title + Select input dropdown field */}
+            {
+              countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
 
           </Select>
       </FormControl>
       </div>
 
-      {/* Header */}
 
-      {/* Title + Select input dropdown field */}
 
       {/* Info boxes */}
       {/* Info boxes */}
