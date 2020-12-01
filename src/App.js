986966faffb59ce8d37a9bb2219549bd85577df5
michael dilulo <snoleopard01001101@gmail.com>
2020-11-30 17:18:05 -0800

    Mockup for Covid-19 Application

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div className="App">
         <h1>Covid-19 Tracker</h1>
+
+      {/* Header */}
+
+      {/* Title + Select input dropdown field */}
+
+      {/* Info boxes */}
+      {/* Info boxes */}
+      {/* Info boxes */}
+
+      {/* Table */}
+
+      {/* Graph */}
+      
+      {/* Map */}
     </div>
   );
 }
