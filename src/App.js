c476c6ec02703f26b3be43dd74ce74e7d8ca837f
michael dilulo <snoleopard01001101@gmail.com>
2020-12-02 11:02:34 -0800

    Data sorts in table by cases

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import InfoBox from './InfoBox';
 import Map from './Map'
 import Table from './Table'
+import {sortData} from './util'
 
 function App() {
   const [countries, setCountries] = useState([])
@@ -30,7 +31,9 @@ function App() {
               name: country.country,
               value: country.countryInfo.iso2
             }))
-          setTableData(data)
+          
+          const sortedData = sortData(data)
+          setTableData(sortedData)
           setCountries(countries)
       })
     }
