f72ba10354bed245328e0ef0405623df194af9dc
michael dilulo <snoleopard01001101@gmail.com>
2020-12-05 10:53:43 -0800

    Zoom, Center, with Map

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
   const [country, setCountry] = useState('worldwide')
   const [countryInfo, setCountryInfo] = useState({})
   const [tableData, setTableData] = useState([])
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 })
+  const[mapZoom, setMapZoom] = useState(3)
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -82,7 +84,7 @@ function App() {
       </div>
       
       {/* Map */}
-      <Map />
+      <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/Map.js
+++ src/Map.js
@@ -3,10 +3,10 @@ import './Map.css'
 // rename Map as LeafletMap
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
-function Map() {
+function Map({center, zoom}) {
     return (
         <div className="map">
-            <LeafletMap>
+            <LeafletMap center={center} zoom={zoom}>
                    <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
