c9580e68cedb62c1948abdf06be8df0ae0bba7e3
michael dilulo <snoleopard01001101@gmail.com>
2020-12-05 10:56:37 -0800

    Map shows

--- src/Map.js
+++ src/Map.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Map.css'
-// rename Map as LeafletMap
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+// Needed to import MapContainer instead of leafletMap, map shows
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function Map({center, zoom}) {
     return (
         <div className="map">
-            <LeafletMap center={center} zoom={zoom}>
+            <MapContainer center={center} zoom={zoom}>
                    <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-            </LeafletMap>
+            </MapContainer>
             <h1>I am a Map</h1>
         </div>
     )
