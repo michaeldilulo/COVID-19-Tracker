96d2b59cccdf8677f67382a7b9e4da178fae216a
michael dilulo <snoleopard01001101@gmail.com>
2020-12-10 12:42:36 -0800

    Popup style when country is clicked

--- src/util.js
+++ src/util.js
@@ -5,7 +5,7 @@ import numeral from "numeral"
 const casesTypeColors = {
     cases: {
         hex: "#CC1034",
-        multiplier: 250,
+        multiplier: 800,
     },
     recovered: {
         hex: "#7dd71d",
@@ -36,12 +36,13 @@ export const showDataOnMap = (data, casesType = 'cases') => (
             }
         >
             <Popup>
-                <div>
-                <div style={{backgroundImage: `url(${country.countryInfo.flag})`}} />
-                    <div>{ country.country}</div>
-                    <div>Cases: {numeral(country.cases).format("0,0")}</div>
-                    <div>Recovered: { numeral(country.recovered).format("0,0")}</div>
-                    <div>Deaths: { numeral(country.deaths).format("0,0")}</div>
+                <div className="info-container">
+                    <div className="info-flag"
+                        style={{ backgroundImage: `url(${country.countryInfo.flag})` }} />
+                    <div className="info-name">{ country.country}</div>
+                    <div className="info-confirmed">Cases: {numeral(country.cases).format("0,0")}</div>
+                    <div className="info-recovered">Recovered: { numeral(country.recovered).format("0,0")}</div>
+                    <div className="info-deaths">Deaths: { numeral(country.deaths).format("0,0")}</div>
                 </div>
             </Popup>
         </Circle>
