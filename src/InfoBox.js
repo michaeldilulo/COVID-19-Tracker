40c87ffbf5f500d0d32f5844e6f2f4582ed31a28
michael dilulo <snoleopard01001101@gmail.com>
2020-12-12 13:23:19 -0800

    Box shows active when selected with border

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import "./InfoBox.css"
 
-function InfoBox({title, cases, total, ...props}) {
+function InfoBox({title, cases, total, active, ...props}) {
     return (
-        <Card onClick={props.onClick}className="infoBox">
+        // -- is for modification of the element
+        <Card onClick={props.onClick}className={`infoBox ${active && "infoBox--selected"}` }>
             <CardContent>
                 <Typography color="textSecondary" className="infoBox__title">
                     {title}
